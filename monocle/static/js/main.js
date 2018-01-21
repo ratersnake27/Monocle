2a40d79cfe4de7ba775b42af1b983086cba233fb
ratersnake27 <35665976+ratersnake27@users.noreply.github.com>
2018-01-22 03:17:00 +0530

    CopyCoordinates Function Fix

--- monocle/static/js/main.js
+++ monocle/static/js/main.js
@@ -322,6 +322,48 @@ if (_DisplaySpawnpointsLayer === 'True') {
     monitor(overlays.Workers, false)
 }
 
+$(document).on('click', '.coords', function(){
+	copyToClipboard($(this).next('input'));
+});
+
+function copyToClipboard(el) {
+
+    // resolve the element
+    el = (typeof el === 'string') ? document.querySelector(el) : el;
+
+    // handle iOS as a special case
+    if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+
+        // save current contentEditable/readOnly status
+        var editable = el.contentEditable;
+        var readOnly = el.readOnly;
+
+        // convert to editable with readonly to stop iOS keyboard opening
+        el.contentEditable = true;
+        el.readOnly = true;
+
+        // create a selectable range
+        var range = document.createRange();
+        range.selectNodeContents(el);
+
+        // select the range
+        var selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        el.setSelectionRange(0, 999999);
+
+        // restore contentEditable/readOnly to original state
+        el.contentEditable = editable;
+        el.readOnly = readOnly;
+    }
+    else {
+        el.select();
+    }
+
+    // execute copy command
+    document.execCommand('copy');
+}
+
 function getPopupContent (item, boost_status) {
     var diff = (item.expires_at - new Date().getTime() / 1000);
     var minutes = parseInt(diff / 60);
